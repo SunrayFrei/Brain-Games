b144a82cffd3a7c3bf92a123d275132e776f4faa
victotia tretiak <120196326+SunrayFrei@users.noreply.github.com>
2023-12-05 18:11:23 +0300

    fixed executables files

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,34 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import playEvenGame from '../src/brain-even.js'
 
 console.log('Welcome to the Brain Games!');
-
-const playGame = () => {
-  console.log('May I have your name?')
-
-  const userName = readlineSync.question('Your answer: ');
-
-  console.log(`Hello, ${userName}!`);
-
-  console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
-
-  let correctAnswers = 0;
-
-  while (correctAnswers < 3) {
-    const number = Math.floor(Math.random() * 100) + 1;
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question(`Your answer: `);
-
-    const isCorrect = number % 2 === 0 ? 'yes' : 'no';
-
-    if (answer.toLowerCase() === isCorrect) {
-      console.log(`Correct!`);
-      correctAnswers += 1;
-    } else if (answer.toLowerCase() !== isCorrect) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isCorrect}'.\nLet's try again, ${userName}!`);
-      correctAnswers += 3;
-    }
-  }
-}
-playGame();
+playEvenGame();
---
+++ src/brain-even.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+
+const playEvenGame = () => {
+  
+  console.log('May I have your name?')
+  const userName = readlineSync.question('Your answer: ');
+  console.log(`Hello, ${userName}!`);
+
+
+  console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+  let correctAnswers = 0;
+
+  while (correctAnswers < 3) {
+    const number = Math.floor(Math.random() * 100) + 1;
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question(`Your answer: `);
+
+    const isCorrect = number % 2 === 0 ? 'yes' : 'no';
+
+    if (answer.toLowerCase() === isCorrect) {
+      console.log(`Correct!`);
+      correctAnswers += 1;
+    } else if (answer.toLowerCase() !== isCorrect) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isCorrect}'.\nLet's try again, ${userName}!`);
+      correctAnswers += 3;
+    }
+  }
+};
+
+export default playEvenGame;
