31e3bcfa0044544de353c6ec10cb3f2c7cf1c009
victotia tretiak <120196326+SunrayFrei@users.noreply.github.com>
2023-12-12 10:14:31 +0300

    added a depo -- bin/games

---
+++ bin/games/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+import playEvenGame from '../../src/brain-even.js'
+playEvenGame();
---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+import playGCDGame from '../../src/games/brain-gcd.js';
+playGCDGame();
