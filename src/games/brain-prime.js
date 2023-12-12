c9e1ab62cf99a3aff5aa93afb834885b61b1b2d0
victotia tretiak <120196326+SunrayFrei@users.noreply.github.com>
2023-12-12 10:24:38 +0300

    fix bugs

---
+++ src/games/brain-prime.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+import getName from '../cli.js';
+import { genNumUnderTen, isPrime, correctnessCheck } from './general.js';
+
+const playPrimeGame = () => {
+  const playerName = getName();
+
+  let continueOrEnd = true;
+  for (let i = 0; i < 3; i += 1) {
+    if (continueOrEnd === false) {
+      return;
+    }
+    const num = genNumUnderTen();
+    const correctAnswer = isPrime(num);
+    console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`);
+    console.log(`Question: ${num}`);
+    const answer = readlineSync.question(`Your answer: `).toLowerCase();
+    const isCorrect = correctnessCheck(answer, correctAnswer, playerName);
+    continueOrEnd = isCorrect;
+  };
+  console.log(`Congratulations, ${playerName}`);
+};
+
+export default playPrimeGame;
