2a9a3f054fdc8841f9ed2520d7d0eda4cf9b4b43
victotia tretiak <120196326+SunrayFrei@users.noreply.github.com>
2023-12-15 13:26:33 +0300

    added game logic files and made corrections

---
+++ src/brain-even.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+import getName from '../cli.js';
+import { genNum, checkCorrectness } from './general.js';
+
+
+const playEvenGame = () => {
+  const playerName = getName();
+  console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+  let continueOrEnd = true;
+  
+  for (let i = 0; i < 3; i += 1) {
+    if (continueOrEnd === false) {
+      return;
+    }
+    const number = genNum();
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question(`Your answer: `).toLowerCase();
+    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+    const isCorrect = checkCorrectness(answer, correctAnswer, playerName);
+    continueOrEnd = isCorrect;
+  };
+  console.log(`Congratulations, ${playerName}`);
+};
+export default playEvenGame;
---
+++ src/brain-gcd.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+import getName from '../cli.js';
+import { genNumUnderTen, checkCorrectness } from './utils.js';
+
+const findGCD = (a, b) => {
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+};
+
+const playGCDGame = () => {
+  const playerName = getName();
+  
+  let continueOrEnd = true;
+  for (let i = 0; i < 3; i += 1) {
+    if (continueOrEnd === false) {
+      return;
+    }
+  console.log(`Find the greatest common divisor of given numbers.`);
+  const num1 = genNumUnderTen();
+  const num2 = genNumUnderTen();
+  const resultGCD = findGCD(num1, num2);
+  console.log(`Question: ${num1} and ${num2}`);
+  const answer = Number(readlineSync.question(`Your answer: `));
+  const isCorrect = checkCorrectness(answer, resultGCD, playerName);
+  continueOrEnd = isCorrect;
+  };
+  console.log(`Congratulations, ${playerName}`);
+};
+
+export default playGCDGame;
