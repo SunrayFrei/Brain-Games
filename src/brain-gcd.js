84957afe9b2f1842e7771fcdceb9754d77eaf978
SunrayFrei <120196326+SunrayFrei@users.noreply.github.com>
2023-12-17 23:45:06 +0300

    Update brain-gcd.js

--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -4,7 +4,7 @@ import { genNumUnderTen, checkCorrectness } from './utils.js';
 
 const findGCD = (a, b) => {
   while (b !== 0) {
-    let temp = b;
+    const temp = b;
     b = a % b;
     a = temp;
   }
@@ -13,21 +13,21 @@ const findGCD = (a, b) => {
 
 const playGCDGame = () => {
   const playerName = getName();
-  
+
   let continueOrEnd = true;
   for (let i = 0; i < 3; i += 1) {
     if (continueOrEnd === false) {
       return;
     }
-  console.log(`Find the greatest common divisor of given numbers.`);
-  const num1 = genNumUnderTen();
-  const num2 = genNumUnderTen();
-  const resultGCD = findGCD(num1, num2);
-  console.log(`Question: ${num1} ${num2}`);
-  const answer = Number(readlineSync.question(`Your answer: `));
-  const isCorrect = checkCorrectness(answer, resultGCD, playerName);
-  continueOrEnd = isCorrect;
-  };
+    console.log(`Find the greatest common divisor of given numbers.`);
+    const num1 = genNumUnderTen();
+    const num2 = genNumUnderTen();
+    const resultGCD = findGCD(num1, num2);
+    console.log(`Question: ${num1} ${num2}`);
+    const answer = Number(readlineSync.question('Your answer: '));
+    const isCorrect = checkCorrectness(answer, resultGCD, playerName);
+    continueOrEnd = isCorrect;
+  }
   console.log(`Congratulations, ${playerName}!`);
 };
 
