2465c0b0cfca54d85b4baf9978d78bf3c68951ab
SunrayFrei <120196326+SunrayFrei@users.noreply.github.com>
2023-12-17 23:57:20 +0300

    Update brain-prime.js

--- src/brain-prime.js
+++ src/brain-prime.js
@@ -4,16 +4,19 @@ import { genNumUnderTen, checkCorrectness } from './utils.js';
 
 const isPrime = (num) => {
   if (num <= 1) {
-      return 'no';
-    }
+    return 'no';
+  }
 
   if (num === 2) {
     return 'yes';
   }
 
-  for (let i = 2; i < num; i += 1) {
-      return num % i === 0 ? 'no' : 'yes'
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return 'no';
     }
+  }
+  return 'yes';
 };
 
 const playPrimeGame = () => {
