9d4072c44402708a3b172b1f988e6766727ff898
victotia tretiak <120196326+SunrayFrei@users.noreply.github.com>
2024-01-04 17:55:02 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,24 +5,24 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (number) => {
   if (number <= 1) {
-    return 'no';
+    return false;
   }
 
   if (number === 2) {
-    return 'yes';
+    return true;
   }
 
   for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
 const playARound = () => {
   const number = getRandomNumber(0, 10);
-  const correctAnswer = isPrime(number);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
   return [number, correctAnswer];
 };
 
